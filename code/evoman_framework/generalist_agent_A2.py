3c0590cd42489d5dee1da89e9da594b50b2e1454
Kaspernicholas <kasperanicholas@gmail.com>
2019-10-13 15:51:11 +0200

    Generalist modified, not working.

--- code/evoman_framework/generalist_agent_A2.py
+++ code/evoman_framework/generalist_agent_A2.py
@@ -28,7 +28,6 @@ import glob
 from deap import base
 from deap import creator
 from deap import tools
-from deap import algorithms
 
 ### Configuration
 experiment_name = 'generalist_A2_fixed'
@@ -37,8 +36,6 @@ if not os.path.exists(experiment_name):
     os.makedirs(experiment_name)
 
 n_hidden_neurons = 10
-
-n_vars = (env.get_num_sensors()+1)*n_hidden_neurons + (n_hidden_neurons+1)*5
 
 domain_upper = 1
 domain_lower = -1
@@ -66,27 +63,10 @@ def evaluation(individual):
     return [(f,), p]
 
 
-# Initialize the fitness and individual classes
-creator.create("FitnessMax", base.Fitness, weights=(1.0,))
-creator.create("Individual", list, fitness=creator.FitnessMax)
-
-toolbox = base.Toolbox()
-# Attribute generator
-toolbox.register("attr_weight", random.uniform, domain_lower, domain_upper)
-# Structure initializers
-toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_weight, n_vars)
-toolbox.register("population", tools.initRepeat, list, toolbox.individual)
-toolbox.register("evaluate", evaluation)
-toolbox.register("mate", tools.cxTwoPoint)
-toolbox.register("mutate", tools.mutFlipBit, indpb=0.05)
-toolbox.register("select", tools.selTournament, tournsize=3)
-
-name_suffix = datetime.now().strftime('%d-%m-%Y_%H-%M-%S')
-
 for en in all_combos:
 
     # Evo framework config
-    # initializes simulation in individual evolution mode, for two static enemies.
+    # initializes simulation in multi evolution mode, for two static enemies.
     env = Environment(experiment_name=experiment_name,
                       enemies=en,
                       multiplemode="yes",
@@ -97,6 +77,25 @@ for en in all_combos:
                       logs="off",
                       speed="fastest")
 
+    n_vars = (env.get_num_sensors() + 1) * n_hidden_neurons + (n_hidden_neurons + 1) * 5
+
+    # Initialize the fitness and individual classes
+    creator.create("FitnessMax", base.Fitness, weights=(1.0,))
+    creator.create("Individual", list, fitness=creator.FitnessMax)
+
+    toolbox = base.Toolbox()
+    # Attribute generator
+    toolbox.register("attr_weight", random.uniform, domain_lower, domain_upper)
+    # Structure initializers
+    toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_weight, n_vars)
+    toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+    toolbox.register("evaluate", evaluation)
+    toolbox.register("mate", tools.cxTwoPoint)
+    toolbox.register("mutate", tools.mutFlipBit, indpb=0.05)
+    toolbox.register("select", tools.selTournament, tournsize=3)
+
+    name_suffix = datetime.now().strftime('%d-%m-%Y_%H-%M-%S')
+
     pop = toolbox.population(n=n_pop)
     hof = tools.HallOfFame(1)
     stats = tools.Statistics(lambda ind: ind.fitness.values)
@@ -106,15 +105,15 @@ for en in all_combos:
     stats.register("max", np.max)
 
     # Update the enemy
-    # env.update_parameter('enemies', [en])
+    env.update_parameter('enemies', [en])
 
-    # print('\n Evolving generalist on enemy: '+str(en)+' \n')
+    print('\n Evolving generalist on enemy: '+str(en)+' \n')
 
     # pop, log = algorithms.eaSimple(pop, toolbox, cxpb=cross_p, mutpb=mutation_p, ngen=n_gens, stats=stats, halloffame=hof, verbose=True)
     pop, log = eaMuPlusLambda(pop, toolbox, mu=mu, lambda_=lambda_, cxpb=cross_p, mutpb=mutation_p, ngen=n_gens, stats=stats, halloffame=hof, verbose=True)
 
     # saves results for first pop
-    f  = open(experiment_name+'/life_test' + name_suffix + '_enemies_' + str(en[0]) + str(en[1]) + '.txt','a')
+    f = open(experiment_name+'/life_test' + name_suffix + '_enemies_' + str(en[0]) + str(en[1]) + '.txt','a')
     f.write(str(log))
     f.write(
         '\n' +
@@ -131,6 +130,6 @@ for en in all_combos:
     )
     f.close()
 
-    f = open(experiment_name+'/results_best_' + name_suffix + '_enemy_' + str(enemy_1) + str(enemy_2) + '.txt','a')
+    f = open(experiment_name+'/results_best_' + name_suffix + '_enemy_' + str(en[0]) + str(en[1]) + '.txt','a')
     f.write(str(hof[0]))
     f.close()
